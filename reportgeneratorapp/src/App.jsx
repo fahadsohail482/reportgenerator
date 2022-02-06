ab0374202840d9b7dd71a66ec605f9da14eb432a
fahadsohail482 <46647496+fahadsohail482@users.noreply.github.com>
2022-02-06 23:15:50 +0500

    report

--- reportgeneratorapp/src/App.jsx
+++ reportgeneratorapp/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import pngImage from "./report-01.png";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>App Here </h1>
+      <img src={pngImage} alt="pcr-medical-report" />
     </div>
   );
 }
